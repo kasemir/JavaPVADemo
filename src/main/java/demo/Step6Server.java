6cbffe9c20fad43001a582f9fa3f608f3ff1393b
kasemir <kay.kasemir@gmail.com>
2022-08-15 16:08:52 -0400

    EPICS_PVA_NAME_SERVERS example

--- src/main/java/demo/Step6Server.java
+++ src/main/java/demo/Step6Server.java
@@ -2,6 +2,7 @@ package demo;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -11,12 +12,22 @@ import org.epics.pva.server.PVAServer;
 import org.epics.pva.server.ServerPV;
 import org.epics.pva.server.WriteEventHandler;
 
-/** PVA server with write access */
+/** PVA server with write access
+ * 
+ *  Test with "pvmonitor demo" and "pvput demo 42"
+ */
 public class Step6Server
 {
     public static void main(String[] args) throws Exception
     {
-        Logger.getLogger("").setLevel(Level.WARNING);
+        // Run on non-default but known TCP port?
+        // System.setProperty("EPICS_PVA_SERVER_PORT", "5085");
+
+        // Log more detail to see network packages
+        Logger logger = Logger.getLogger("");
+        logger.setLevel(Level.ALL);
+        for (Handler handler : logger.getHandlers())
+            handler.setLevel(logger.getLevel());
 
         try
         (   PVAServer server = new PVAServer() )
@@ -45,3 +56,27 @@ public class Step6Server
         }
     }
 }
+
+
+/*  For TCP-only client demo:
+ * 
+ *  1) Run this server
+ *     Take note of the port number listed in the message.
+ *     If this is the only PVA server, it'll be 5075:
+ * 
+ *     CONFIG: Listening on TCP .........:5075
+ * 
+ *     Otherwise, it will be some random free port.
+ *     Could set EPICS_PVA_SERVER_PORT=5085 to place
+ *     server on known non-default port...
+ *     
+ *  2) Run PVA client like this, using port name obtained above:
+ * 
+ *     EPICS_PVA_NAME_SERVERS=127.0.0.1:5075   \
+ *     EPICS_PVA_AUTO_ADDR_LIST=no             \
+ *     pvxget demo
+ * 
+ *     Must use "pvxget" from PVXS instead of the "pvget" from older C++
+ *     implementation which lacks NAME_SERVERS support!
+ */
+
